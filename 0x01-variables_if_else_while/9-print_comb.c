efa245fe7552b1ac48f01746acb6e6a3ff82b098
AbelT <abelteame22@gmail.com>
2022-07-01 05:57:23 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (int b = 48; b <= 57; b++)
+		{
+			putchar((char) a);
+			putchar((char) b);
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	return (0);
+}
