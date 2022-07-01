2850d659a17267c4ea97867ab8492b86fca35b18
AbelT <abelteame22@gmail.com>
2022-07-01 05:41:43 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 102;)
+	{
+		putchar((char) a);
+		if (a == 57)
+			a += 97 - 57;
+		else
+			a++;
+	}
+	putchar('\n');
+	return (0);
+}
