1be77905c56298ef300441bd2c42094f54c99579
AbelT <abelteame22@gmail.com>
2022-07-05 09:06:01 -0700

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * times_table - prints multiplication table
+ *
+ * Return: no return value
+ */
+void times_table(void)
+{
+	int a, b, val, first, second;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			val = a * b;
+	
+			if (b > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (val > 9)
+			{
+				first = val / 10;
+				second = val % 10;
+
+				_putchar(first + '0');
+				_putchar(second + '0');
+			} else
+			{
+				_putchar(' ');
+				_putchar(val + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
