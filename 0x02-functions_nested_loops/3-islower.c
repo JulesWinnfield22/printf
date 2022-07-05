9ab84240aca1270ebbb095953b0b82ce98759f39
AbelT <abelteame22@gmail.com>
2022-07-05 05:12:29 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,12 +4,14 @@
 
 /**
  * _islower - checks for lowercase alphabet
+ * @c: the character to be checked
  *
  * Return: 0 or 1
  */
 int _islower(int c)
 {
-	if (c > 96 && c < 123) return 1;
+	if (c > 96 && c < 123)
+		return (1);
 
 	return (0);
 }
