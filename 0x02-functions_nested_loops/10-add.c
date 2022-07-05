6374e1ebb1c59b97b6f5a449d48c9e5f3d0300e8
AbelT <abelteame22@gmail.com>
2022-07-05 09:20:44 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+#include "main.h"
+
+/**
+ * add - addes two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: any number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
