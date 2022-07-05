92a299f1b6eb9448960fbf74dbc8fe5bc4dc25aa
AbelT <abelteame22@gmail.com>
2022-07-05 12:22:12 -0700

    fibi

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,11 +10,13 @@ int main(void)
 {
 	long int a, b, i, t;
 
-	a = 1, b = 0;
+	a = 1, b = 1;
 
 	for (i = 0; i < 50; i++)
 	{
-		printf("%lu, ", a);
+		printf("%lu", a);
+		if (i < 49)
+			printf(", ");
 
 		t = b;
 		b = a;
