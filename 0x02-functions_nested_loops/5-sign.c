470b5c6ee4a2582ca4500fcac354715d43a500e2
AbelT <abelteame22@gmail.com>
2022-07-05 05:48:58 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of the number
+ * @c: the number which the sign will be printed for
+ *
+ * Return: 0 or -1 or 1
+ */
+int print_sign(int c)
+{
+	if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	} else if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	putchar('-');
+	return (-1);
+}
+
