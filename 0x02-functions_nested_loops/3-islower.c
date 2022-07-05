aa3e08d1b9f48a1693c5e3ea6a45d6152c5a70b4
AbelT <abelteame22@gmail.com>
2022-07-05 05:07:17 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase alphabet
+ *
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123) return 1;
+
+	return (0);
+}
