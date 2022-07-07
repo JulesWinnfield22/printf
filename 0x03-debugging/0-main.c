0a80f0df58f1ab1c301a0d452f73d3ae9514542c
AbelT <abelteame22@gmail.com>
2022-07-07 02:59:15 -0700

    pos neg

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * positive_or_negative - prints a numbers positiviness, negativeness, zeroness
+ * @i: the number
+ *
+ * Resturn: no return value
+ */
+void positive_or_negative(int i)
+{
+	if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i > 0)
+		printf("%d is positive\n", i);
+	else
+		printf("%d is negative\n", i);
+}
+
