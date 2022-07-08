289b182f65bd9e400076e9df3adb4f70e26162d1
AbelT <abelteame22@gmail.com>
2022-07-08 05:49:13 -0700

    fizzzzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
+* Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a < 101; a++)
+	{
+		if (a % 5 == 0 && a % 3 == 0)
+			printf("FizzBuzz ");
+		else if (a % 5 == 0)
+			printf("Buzz ");
+		else if (a % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+
+	return (0);
+}
