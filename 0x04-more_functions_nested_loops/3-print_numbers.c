cc4c802ab7086b70c819ea5c01064a8238aed0b7
AbelT <abelteame22@gmail.com>
2022-07-08 01:58:00 -0700

    print #

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ *
+ * Return: no return value
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a)
+	}
+	_putchar('\n');
+}
