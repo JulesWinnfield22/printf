6c004b0df3cb99e4cfa6d7c9b5eb663192f9cd0a
AbelT <abelteame22@gmail.com>
2022-07-11 11:45:12 -0700

    last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * len - length of a string
+ * @s: the string
+ *
+ * Return: an int value
+ */
+int len(char *s)
+{
+	int a;
+	char c;
+
+	a = 0, c = s[0];
+
+	while (c != '\0')
+	{
+		a++;
+		c = s[a];
+	}
+
+	return (a);
+}
+/**
+ * puts_half - prints the last half of the string to stdout
+ * @s: the sring
+ *
+ * Return: no return value
+ */
+void puts_half(char *s)
+{
+
+	int length, a;
+	char c;
+
+	length = len(s);
+	c = s[length / 2];
+	a = length / 2;
+
+	while (a < length)
+	{
+		_putchar(c);
+		a++;
+		c = s[a];
+	}
+	_putchar('\n');
+}
