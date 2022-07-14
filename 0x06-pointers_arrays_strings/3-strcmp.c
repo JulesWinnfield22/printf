f557f6f932202ed10bac12df1057b0a026802ab0
AbelT <abelteame22@gmail.com>
2022-07-14 07:21:21 -0700

    string cmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * len - the length of the string
+ * @s: the string
+ *
+ * Return: any integer greater than or eqal to 0
+ */
+int len(char *s)
+{
+	int length;
+	char c;
+
+	length = 0, c = s[0];
+
+	while (c != '\0')
+	{
+		length++;
+		c = s[length];
+	}
+
+	return (length);
+}
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 15 or -15 or 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int val, a;
+
+	val = 0;
+
+	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
+	{
+		if (s1[a] > s2[a])
+			val = 15;
+		else if (s1[a] < s2[a])
+			val = -15;
+		else
+			continue;
+		break;
+	}
+	return (val);
+}
