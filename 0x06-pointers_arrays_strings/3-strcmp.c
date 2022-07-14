6cf1e3e858eeefc8f3bd40efddd8c80612b0668b
AbelT <abelteame22@gmail.com>
2022-07-14 07:24:56 -0700

    string c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -33,7 +33,7 @@ int _strcmp(char *s1, char *s2)
 
 	val = 0;
 
-	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
 	{
 		if (s1[a] > s2[a])
 			val = 15;
