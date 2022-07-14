f74f130e055a9f0cafcf82f153a7ecee913c6e07
AbelT <abelteame22@gmail.com>
2022-07-14 09:35:38 -0700

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * has - checks to see if a string contains an item
+ * @c: character
+ *
+ * Return: 1 or 0
+ */
+int has(char c)
+{
+	char *s = " \t\n,;.!?\"(){}";
+	int a;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		if (c == s[a])
+			return (1);
+		a++;
+	}
+
+	return (0);
+}
+
+/**
+ * cap_string - capitalize all words
+ * @s: the string
+ *
+ * Return: the changed string
+ */
+char *cap_string(char *s)
+{
+	int a;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		if (has(s[a]) == 1 && s[a + 1] >= 97 && s[a + 1] <= 122)
+			s[a + 1] = s[a + 1] - 32;
+		a++;
+	}
+	return (s);
+}
