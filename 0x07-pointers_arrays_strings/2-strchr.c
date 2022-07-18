5a4128a67ea9fd8d85daf3bb8838c0e282c53e26
AbelT <abelteame22@gmail.com>
2022-07-18 06:12:45 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,17 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int a, f;
+	int a;
 
-	f = 0;
 	for (a = 0; s[a] != '\0'; a++)
 	{
 		if (s[a] == c)
-		{
-			f = 1;
 			break;
-		}
 	}
 
-	return (f ? s + a : 0);
+	return (*(s + a) ? s + a : 0);
 }
