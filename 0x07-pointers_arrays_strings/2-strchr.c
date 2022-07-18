ddee29105ca6f5c9f8d1bb8611f12aa47737da87
AbelT <abelteame22@gmail.com>
2022-07-18 05:39:18 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
- #include <stddef.h>
 /**
  * _strchr -  locates a character in a string.
  * @s: the string
@@ -17,5 +16,5 @@ char *_strchr(char *s, char c)
 			break;
 	}
 
-	return (*(s + a) ? s + a : NULL);
+	return (*(s + a) ? s + a : 0);
 }
