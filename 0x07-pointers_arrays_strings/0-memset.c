dcf64c4f2fea52ad34f316ee66ff577a5712e887
AbelT <abelteame22@gmail.com>
2022-07-18 04:49:19 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,8 +10,14 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n--)
-		*s++ = (unsigned char)b;
+	unsigned int a;
+
+	a = 0;
+	while (a < n)
+	{
+		s[a] = b;
+		a++;
+	}
 
 	return (s);
 }
