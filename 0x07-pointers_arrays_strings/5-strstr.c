79b8d48efd2ce492f912d153352069ee51ff35d5
AbelT <abelteame22@gmail.com>
2022-07-19 04:48:26 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strstr - gets the substring
+ * @haystack: the string
+ * @needle: the substring
+ *
+ * Return: the substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a, h, n, length;
+
+	a = 0, h = 0, n = 0, length = 0;
+
+	while (needle[length] != '\0')
+		length++;
+
+	while (haystack[a] != '\0')
+	{
+		if (haystack[a] == needle[n])
+		{
+			h = a, n = 0;
+			while (haystack[h] == needle[n])
+			{
+				h++;
+				n++;
+			}
+
+			if ((n - 1) == length)
+				return (haystack + a);
+		}
+		a++;
+	}
+
+	return (0);
+}
