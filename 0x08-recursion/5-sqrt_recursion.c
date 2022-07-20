a2d89ee75c6a56a23cf9c83bdc5e0c6ebf0bbd4c
AbelT <abelteame22@gmail.com>
2022-07-20 06:33:16 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,7 +15,7 @@ int sqrt_recursion(int sqrt, int val)
 	if ((val * val) == sqrt)
 		return (val);
 
-	return (check(sqrt, --val));
+	return (sqrt_recursion(sqrt, --val));
 }
 /**
  * _sqrt_recursion - the sqrt of a number
@@ -28,5 +28,5 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1)
 		return (1);
-	return (check(n, n / 2));
+	return (sqrt_recursion(n, n / 2));
 }
