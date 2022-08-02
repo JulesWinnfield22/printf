2f6f0eca46024c89c3d0c97361a4e5d1a3303019
AbelT <abelteame22@gmail.com>
2022-08-02 05:43:38 -0700

    structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - about dog
+ * @name: the dogs name
+ * @age: the dogs age
+ * @owner: the dogs owner
+ *
+ * Description: structure containing dogs info
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
