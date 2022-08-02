1c0f3e71622f6067cf72e8083c241fe06b4329af
AbelT <abelteame22@gmail.com>
2022-08-02 04:05:58 -0700

    processor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @ac: arg count
+ * @av: args
+ * Return: Always 0.
+ */
+int main(int ac __attribute__((unused)), char *av[])
+{
+	printf("%s\n", *av += 2);
+	return (0);
+}
