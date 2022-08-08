6e8fc3cf832ce86b11a65d7c1c7bd9c5a27ddcd7
AbelT <abelteame22@gmail.com>
2022-08-08 07:27:07 -0700

    calc

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,16 @@
 #ifndef VARIA
 #define VARIA
+
+/**
+ * fmt - types of data
+ * @f: shor cut for the data type
+ *
+ * Description: data type
+ */
+struct fmt {
+	char f;
+};
+void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
