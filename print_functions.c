245d5a41fff848446ed744c0bb2386cbcbdc381b
AbelT <abelteame22@gmail.com>
2022-08-10 11:22:54 -0700

    _printf

--- print_functions.c
+++ print_functions.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * print_string - prints strings
  * @arg: e string
@@ -25,6 +26,56 @@ int print_string(va_list arg)
 	return (a);
 }
 
+/**
+ * printInt - prints integers
+ * @n: the integer
+ * @length: the integer length
+ *
+ * Return: the length
+ */
+int printInt(int n, int length)
+{
+	if (n < 0)
+	{
+		++length;
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		++length;
+	}
+
+	if (n / 10)
+		length = printInt(n / 10, ++length);
+
+	_putchar((n % 10) + '0');
+
+	return (1 + length);
+}
+
+/**
+ * print_int - prints an integer
+ * @arg: the int
+ *
+ * Return: the interger length
+ */
+int print_int(va_list arg)
+{
+	int a, length;
+
+	a = va_arg(arg, int);
+	if (!a)
+		return (-1);
+
+	length = 0;
+
+	length += printInt(a, length);
+
+	return (length);
+}
 /**
  * print_char - prints a char
  * @arg: the char
