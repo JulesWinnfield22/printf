b9d75b8e830ef7919f89ff7bdbe27ab505096b44
AbelT <abelteame22@gmail.com>
2022-08-10 08:49:07 -0700

    _printf

---
+++ get_func.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * get_func - gets the function to execute the operation
+ * @c: the flag
+ *
+ * Return: the function
+ */
+
+int (*get_func(char c))(va_list)
+{
+	int a;
+	struct operation ops[] = {
+		{'s', print_string},
+		{'c', print_char},
+	};
+
+	a = 0;
+	while (ops[a].flag != 0)
+	{
+		if (ops[a].flag == c)
+			return (ops[a].func);
+		a++;
+	}
+
+	return (0);
+}
+
