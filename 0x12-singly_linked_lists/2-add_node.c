a4b156942558da1d3286743b393641fc591a4f30
AbelT <abelteame22@gmail.com>
2022-08-11 03:49:51 -0700

    singlr linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -30,15 +30,16 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	if (!str)
+	if (!*str)
 		return (0);
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == 0)
 		return (0);
 	new->str = strdup(str);
 	new->len = get_length(strdup(str));
 	new->next = *head;
 	*head = new;
+
 	return (*head);
 }
