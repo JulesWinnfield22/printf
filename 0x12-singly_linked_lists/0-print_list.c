3fc37f77a8e36a8bc93e00dc276d540ff38f3ba6
AbelT <abelteame22@gmail.com>
2022-08-11 02:23:49 -0700

    linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: the list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int a;
+
+	a = 0;
+
+	while (1)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		a++;
+		if (h->next == NULL)
+			break;
+		h = h->next;
+	}
+
+	return (a);
+}
