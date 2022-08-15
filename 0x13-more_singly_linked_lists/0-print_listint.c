887a66a4cf0cd2b0a19ce35403cc6972d2e36c0e
AbelT <abelteame22@gmail.com>
2022-08-15 01:35:34 -0700

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @listint_t: the list
+ *
+ * Return: the number of elemets
+ */
+size_t print_listint(const listint_t *h)
+{
+	int length;
+
+	length = 0;	
+	while (h)
+	{
+		++length;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (length);
+}
