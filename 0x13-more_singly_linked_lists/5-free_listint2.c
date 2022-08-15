dffbdab1f65607f4925aa5bf445a6891098ce0c6
AbelT <abelteame22@gmail.com>
2022-08-15 03:14:58 -0700

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees memory
+ * @head: the list
+ *
+ * Return: no return value
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = (*head);
+		*head = (*head)->next;
+		free(tmp);
+	}
+}
